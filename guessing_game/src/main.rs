73a76f44467646e857426e9048c7da54f8e944ae
noreflection <a191a191a@gmail.com>
2019-12-06 23:19:37 +0300

    init

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("input guess:");
+
+    loop {
+        let mut guess = String::new();
+
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+
+        io::stdin().read_line(&mut guess)
+            .expect("failed to read an input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("secret is: {}", secret_number);
+        println!("your guess is: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("secret is less than guess!"),
+            Ordering::Greater => println!("secret is greater than guess!"),
+            Ordering::Equal => {
+                println!("secret equals the guess!");
+                break;
+            }
+        }
+    }
+}
